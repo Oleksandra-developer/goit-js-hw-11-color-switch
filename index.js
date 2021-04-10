cd0229a0a6dcb0e2c5cb5bfbdce5e5fd59336c51
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-04-10 22:02:57 +0300

    Мигание цветов

---
+++ index.js
@@ -0,0 +1,42 @@
+// Переключатель цветов
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+]
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="stop"]'),
+    bodyPage: document.querySelector("body"),
+    isActive: false
+}
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let colorBlinking = null;
+const changeColor = refs.startBtn.addEventListener("click", () => {    
+    if (refs.isActive = true) {
+        return
+    }
+    colorBlinking = setInterval(() => {
+        const randomIntegerFromInterval = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        refs.bodyPage.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+        console.log(colors[randomIntegerFromInterval(0, colors.length)])
+        refs.isActive = true;
+    }, 1000);    
+    return
+})
+
+refs.stopBtn.addEventListener("click", () => {
+    refs.isActive = false;
+    clearInterval(colorBlinking);
+    })
+    
+
